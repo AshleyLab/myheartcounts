94dfa7add527626604ea6a67d2fe16d7a8f38374
Rachel <rlgoldfeder@gmail.com>
2015-04-07 09:14:44 -0700

    added sleep debt vs happiness, parQ vs diet glm, dailycheck: activity vs sleep

--- myheart.R
+++ myheart.R
@@ -146,8 +146,91 @@ ggplot(dietSleep.ct, aes(as.factor(sleep_debt),fruitVeggie)) +geom_boxplot() +xl
 
 
 # vs moderate + vigorous activity
-exerciseSleep.clean = data.frame(exercise = dietSleep.clean$vigorous_act+dietSleep.clean$moderate_act, sleep_debt = dietSleep.clean$sleep_time1 - dietSleep.clean$sleep_time)
-#exerciseSleep.clean.sm = exerciseSleep.clean$exercise < 
+exerciseSleep.clean.sm = data.frame(exercise = dietSleep.clean$vigorous_act+dietSleep.clean$moderate_act, sleep_debt = dietSleep.clean$sleep_time1 - dietSleep.clean$sleep_time)
 exerciseSleep.ct = count(exerciseSleep.clean.sm)
 ggplot(exerciseSleep.ct, aes(as.factor(sleep_debt),log(exercise))) +geom_boxplot() +xlab("Average Sleep Debt (hrs)") +ylab("Log(Minutes of Moderate + Vigorous exercise per week)") +theme_bw()
+
+
+#sleep debt vs happiness
+exerciseSleep.satisfied <- merge(dietSleep, satisfied.table, by="healthCode")
+exerciseSleep.satisfied.clean = exerciseSleep.satisfied[exerciseSleep.satisfied$sleep_time1>0 & exerciseSleep.satisfied$sleep_time1<24 &exerciseSleep.satisfied$sleep_time>0 & exerciseSleep.satisfied$sleep_time<24 ,]
+
+
+exerciseSleep.satisfied.sm=data.frame(sleep_debt =exerciseSleep.satisfied.clean$sleep_time1 - exerciseSleep.satisfied.clean$sleep_time, happy=exerciseSleep.satisfied.clean$satisfiedwith_life)
+exerciseSleep.satisfied.ct = count(exerciseSleep.satisfied.sm)
+ggplot(exerciseSleep.satisfied.ct, aes(as.factor(sleep_debt),happy)) +geom_boxplot() +xlab("Average Sleep Debt (hrs)") +ylab("Satisfied with Life") +theme_bw()
+#no association with happiness...
+
+
+
+
+
+
+
+
+
+
+### APRIL 7th 2015 ###
+
+
+# join parQ with risk factors and diet based on healthcodeID
+
+# notes: lots of people answered the parQ (27K, only 16.8k answered risk factor and 16.8k answered diet)
+# lets keep only one row per person
+# join and only keep people who answered all 3 of these surveys.
+
+#distinct each file
+diet.distinct = distinct(diet.table, healthCode)
+parQ.distinct = distinct(parQ.table, healthCode)
+riskFactors.distinct = distinct (riskFactors.table, healthCode)
+
+#join
+dietparQ <- merge(diet.distinct,parQ.distinct, by="healthCode")
+diet.parQ.risk <- merge(dietparQ, riskFactors.distinct, by="healthCode")  #16339 people here. 41 cols.
+
+#glms:
+
+###diet & parQ:
+glm(chestPainInLastMonth~fish+fruit+grains+sugar_drinks+vegetable,data=diet.parQ.risk, family=binomial) #need family=binomial for logistic
+#kinda cool - fish, fruit, and veggies are neg and grains and sugar drinks are positive (all are TINY of course)
+
+glm(dizziness~fish+fruit+grains+sugar_drinks+vegetable,data=diet.parQ.risk, family=binomial)
+# fruit, grains, and sugar are now positive - maybe bc of the sugar in fruit?
+
+#others...
+glm(heartCondition~fish+fruit+grains+sugar_drinks+vegetable,data=diet.parQ.risk, family=binomial)
+glm(jointProblem~fish+fruit+grains+sugar_drinks+vegetable,data=diet.parQ.risk, family=binomial)
+glm(physicallyCapable~fish+fruit+grains+sugar_drinks+vegetable,data=diet.parQ.risk, family=binomial)
+glm(prescriptionDrugs~fish+fruit+grains+sugar_drinks+vegetable,data=diet.parQ.risk, family=binomial)
+glm(chestPain~fish+fruit+grains+sugar_drinks+vegetable,data=diet.parQ.risk, family=binomial) 
+
+#lasso?
+#install.packages('glmnet')
+#library(glmnet)
+#glmnet(chestPainInLastMonth~fish+fruit+grains+sugar_drinks+vegetable,data=diet.parQ.risk, family=binomial)
+
+#do something with risk data. it is just really annoying to deal with. come back and parse it properly.
+
+
+
+
+###dailycheck: is amt of activity correlated with sleep time?
+
+dailyCheck.uniq = as.data.frame(distinct(as.data.frame(cbind(dailyCheck.table$createdOn, dailyCheck.table$healthCode, dailyCheck.table$activity1_time,dailyCheck.table$activity2_time, dailyCheck.table$sleep_time))))
+dailyCheck.uniq$V3=as.numeric(as.character(dailyCheck.uniq$V3))
+dailyCheck.uniq$V4=as.numeric(as.character(dailyCheck.uniq$V4))
+dailyCheck.uniq[is.na(dailyCheck.uniq)] <-0
+dailyCheck.sm = data.frame(total_act = dailyCheck.uniq$V3 + dailyCheck.uniq$V4, sleep = dailyCheck.uniq$V5)
+
+#correlation between sleep and activity?
+cor(dailyCheck.sm$total_act, as.numeric(as.character(dailyCheck.sm$sleep)))
+hist(log(dailyCheck.sm$total_act))
+hist(as.numeric(as.character(dailyCheck.sm$sleep)))
+
+# i wonder if i should remove the 60s from the sleep? maybe that is the default (ie: 1 hr... meaning that nothing was actually selected?)
+dailyCheck.sm.no60 = dailyCheck.sm[!(dailyCheck.sm$sleep==60),]
+hist(log(dailyCheck.sm.no60$total_act))
+hist(as.numeric(as.character(dailyCheck.sm.no60$sleep)))
+cor(dailyCheck.sm.no60$total_act, as.numeric(as.character(dailyCheck.sm.no60$sleep)))
+
 
