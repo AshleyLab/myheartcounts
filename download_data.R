cfd6e8ce85c596d9f2e5f848b82d706bd49923ca
daryl_waggott <dwaggott@gmail.com>
2015-04-08 09:49:49 -0700

    separated survey and blobs into functions; sent survey output to csv files; need to switch the csv to sqlite

--- download_data.R
+++ download_data.R
@@ -1,61 +1,70 @@
-### PREAMBLE
+### PREAMBLE ######################################################################################
 
+require(RSQLite)
 require(synapseClient)
-synapseCacheDir("/home/common/myheart/data/synapseCache") 
+
+data.path <- "/home/common/myhealth/data/"
+cache.path <- "/home/common/myhealth/data/synapseCache"
+
+synapseCacheDir(cache.path) 
 synapseLogin()
 
-### DATA
+### DATA ##########################################################################################
 
-# query your project for all of the tables
-projectId <- "syn3270436"
-q <- synQuery(paste("SELECT id, name FROM table WHERE parentId=='", projectId, "'", sep=""))
+### survey data
 
-if (F) {
-# download survey function
-download.survey <- function(i,q) {
-    cat(paste0("* DOWNLOAD TABLE: ", q[i,"table.name"],"\n"))
-    sq <- synTableQuery(paste('SELECT * FROM ', q[i,"table.id"]) )
+sync.survey <- function() {
+    # query your project for all of the tables
+    projectId <- "syn3270436"
+    q <- synQuery(paste("SELECT id, name FROM table WHERE parentId=='", projectId, "'", sep=""))
+
+    # download survey function
+    download.survey <- function(i,q) {
+        cat(paste0("* DOWNLOAD TABLE: ", q[i,"table.name"],"\n"))
+        sq <- synTableQuery(paste('SELECT * FROM ', q[i,"table.id"]), filePath = paste0(data.path,"/tables/",[i,"table.name"],".csv"))
+        }
+
+    # download all surveys
+    sq.all <- mclapply(as.list(1:nrow(q)), download.survey, q, mc.cores=5)
     }
 
-# download all surveys
-sq.all <- mclapply(as.list(1:nrow(q)), download.survey, q, mc.cores=5)
-}
+sync.survey()
 
+### blob / accelerometer data
 
-# download blob funciton
-#    mclapply(as.list(rownames(tq@values)), function(rn){
-#        synDownloadTableFile(tq, rn, cc)
-#        })
+sync.blob <- function(x) {
+    # check which columns are filehandle function
+    is.filehandle.col <- function(k,sc) {
+        if(sc[[k]]@columnType=="FILEHANDLEID"){
+            return(sc[[k]]@name)
+            } 
+        else{
+            return(NULL)
+            }
+        }
 
+    for (j in c('syn3458480','syn3420486')) {
+        cat(paste0("* DOWNLOAD TABLE: ", j,"\n"))
 
-# check which columns are filehandle function
-is.filehandle.col <- function(k,sc) {
-    if(sc[[k]]@columnType=="FILEHANDLEID"){
-        return(sc[[k]]@name)
-        } 
-    else{
-        return(NULL)
+        # query table for column annotation
+        #tq <- synTableQuery(paste('SELECT * FROM ', j, ' limit 2'))
+        tq <- synTableQuery(paste('SELECT * FROM ', j), filePath = file.path(tempdir(), paste0(j,".csv")))
+
+        # get column data from a particular survey
+        sc <- synapseClient:::synGetColumns(tq@schema)
+
+        # check which columns are filehandles
+        theseCols <- sapply(as.list(1:length(sc)), is.filehandle.col,sc)
+        theseCols <- unlist(theseCols)
+
+        # download 
+        for (col in theseCols) {
+            cat(paste0("* DOWNLOAD BLOB: ", col,"\n"))
+            theseFiles[[col]] <-  mclapply(as.list(rownames(tq@values)), function(rn,col,tq) {tryCatch(synDownloadTableFile(tq, rn, col), error=function(e) NA)}, col,tq,mc.cores=20)
+            }
+
         }
     }
 
-for (j in c('syn3458480','syn3420486')) {
-    cat(paste0("* DOWNLOAD TABLE: ", j,"\n"))
+sync.blob()
 
-    # query table for column annotation
-    #tq <- synTableQuery(paste('SELECT * FROM ', j, ' limit 2'))
-    tq <- synTableQuery(paste('SELECT * FROM ', j))
-
-    # get column data from a particular survey
-    sc <- synapseClient:::synGetColumns(tq@schema)
-
-    # check which columns are filehandles
-    theseCols <- sapply(as.list(1:length(sc)), is.filehandle.col,sc)
-    theseCols <- unlist(theseCols)
-
-    # download 
-    for (col in theseCols) {
-        cat(paste0("* DOWNLOAD BLOB: ", col,"\n"))
-        theseFiles[[col]] <-  mclapply(as.list(rownames(tq@values)), function(rn,col,tq) {tryCatch(synDownloadTableFile(tq, rn, col), error=function(e) NA)}, col,tq,mc.cores=20)
-        }
-
-    }
