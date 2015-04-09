1533046f3752b272f3a500016338c316d2caaf12
daryl_waggott <dwaggott@gmail.com>
2015-04-09 16:30:17 -0700

    added feature for skipping sync of already downloaded blobs

--- download_data.R
+++ download_data.R
@@ -65,7 +65,12 @@ sync.blob <- function(x) {
         theseFiles <- list()
         for (col in theseCols) {
             cat(paste0("* DOWNLOAD BLOB: ", col,"\n"))
-            theseFiles[[col]] <-  mclapply(as.list(rownames(tq@values)), function(rn,col,tq) {tryCatch(synDownloadTableFile(tq, rn, col), error=function(e) NA)}, col,tq,mc.cores=20)
+            get_blob_filename <- function(file.code) {
+                ped.cache.dir <-  paste0(cache.path, "/", as.numeric(substr(file.code, 5,8)),"/", as.numeric(file.code))
+                return(ped.cache.dir)
+                }
+            #theseFiles[[col]] <-  mclapply(as.list(rownames(tq@values)), function(rn,col,tq) {browser();file.code <- tq[rn,col]; if(!file.exists(get_blob_filename(file.code))){return(NA)}else{tryCatch(synDownloadTableFile(tq, rn, col), error=function(e) NA)}}, col,tq,mc.cores=20)
+            theseFiles[[col]] <-  lapply(as.list(rownames(tq@values)), function(rn,col,tq) {file.code <- tq@values[rn,col];if(file.exists(get_blob_filename(file.code))){return(NA)}else{cat(paste0("* DOWNLOADING BLOB ROW: ", rn,"\n"));tryCatch(synDownloadTableFile(tq, rn, col), error=function(e) NA)}}, col,tq)
             }
 
         }
