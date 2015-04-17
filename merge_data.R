912224fd63fae4c800e2bc3fbb49c265321b2ebc
daryl_waggott <dwaggott@gmail.com>
2015-04-17 09:42:24 -0700

    updates for table1

--- merge_data.R
+++ merge_data.R
@@ -9,15 +9,30 @@ quiz <- read.table("../../data/tables/cardiovascular-par-q quiz-v1.tsv",header=T
 day1 <- read.table("../../data/tables/cardiovascular-day_one-v1.tsv",header=T,as.is=T,sep="\t")
 diet <- read.table("../../data/tables/cardiovascular-Diet_survey_cardio-v1.tsv",header=T,as.is=T,sep="\t")
 
+### HEARTAGE CLEANING #############################################################################
+
+heart.age$age = as.numeric(difftime(Sys.Date(),strptime(heart.age$heartAgeDataAge,"%Y-%m-%d"), units="weeks"))/52
+heart.age$heartAgeDataGender[!heart.age$heartAgeDataGender%in%c("[HKBiologicalSexMale]", "[HKBiologicalSexFemale]")] = NA
+
 heart.age$heartAgeDataLdl <- as.numeric(heart.age$heartAgeDataLdl)
 heart.age$heartAgeDataHdl <- as.numeric(heart.age$heartAgeDataHdl)
 
-heart.age[heart.age$heartAgeDataTotalCholesterol<10 | heart.age$heartAgeDataTotalCholesterol>500,"heartAgeDataTotalCholesterol"] <- NA
-heart.age[!is.na(heart.age$heartAgeDataHdl) &  (as.numeric(heart.age$heartAgeDataHdl)<10 | as.numeric(heart.age$heartAgeDataHdl)>500),"heartAgeDataHdl"] <- NA
-heart.age[!is.na(heart.age$heartAgeDataLdl) &  (as.numeric(heart.age$heartAgeDataLdl)<10 | as.numeric(heart.age$heartAgeDataLdl)>500),"heartAgeDataLdl"] <- NA
+# set outliers to null
+heart.age[heart.age$heartAgeDataTotalCholesterol<50 | heart.age$heartAgeDataTotalCholesterol>400,"heartAgeDataTotalCholesterol"] <- NA
+heart.age[!is.na(heart.age$age) & (heart.age$age<18 | heart.age$age>120),"age"] <- NA
+heart.age[!is.na(heart.age$heartAgeDataHdl) &  (as.numeric(heart.age$heartAgeDataHdl)<10 | as.numeric(heart.age$heartAgeDataHdl)>120),"heartAgeDataHdl"] <- NA
+heart.age[!is.na(heart.age$heartAgeDataLdl) &  (as.numeric(heart.age$heartAgeDataLdl)<10 | as.numeric(heart.age$heartAgeDataLdl)>360),"heartAgeDataLdl"] <- NA
 heart.age[!is.na(heart.age$heartAgeDataBloodGlucose) &  (heart.age$heartAgeDataBloodGlucose<1 | heart.age$heartAgeDataBloodGlucose>500),"heartAgeDataBloodGlucose"] <- NA
-heart.age[heart.age$bloodPressureInstruction<50 | heart.age$bloodPressureInstruction>220,"bloodPressureInstruction"] <- NA
+heart.age[heart.age$bloodPressureInstruction<60 | heart.age$bloodPressureInstruction>220,"bloodPressureInstruction"] <- NA
 heart.age[!is.na(heart.age$heartAgeDataSystolicBloodPressure) & (heart.age$heartAgeDataSystolicBloodPressure<50 | heart.age$heartAgeDataSystolicBloodPressure>220),"heartAgeDataSystolicBloodPressure"] <- NA
-heart.age[!is.na(heart.age$bloodPressureInstruction) & (heart.age$bloodPressureInstruction<50 | heart.age$bloodPressureInstruction>220),"heartAgeDataSystolicBloodPressure"] <- NA
+heart.age[!is.na(heart.age$bloodPressureInstruction) & (heart.age$bloodPressureInstruction<60 | heart.age$bloodPressureInstruction>220),"heartAgeDataSystolicBloodPressure"] <- NA
 heart.age$age <-  round((as.numeric(difftime(Sys.Date(), strptime(heart.age$heartAgeDataAge, "%Y-%m-%d"), units="days"))/365.15),2)
 heart.age$mpb <-  round(heart.age$heartAgeDataSystolicBloodPressure + 1/3*(heart.age$bloodPressureInstruction -  heart.age$heartAgeDataSystolicBloodPressure),2)
+
+
+# Filter gender
+
+# take first value for duplicate healthcodes
+
+
+# write output
