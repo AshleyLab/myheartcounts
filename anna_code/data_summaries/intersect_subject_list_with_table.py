eab5baa000b943a1219d64b635bf8fa03a9c14d3
Anna Shcherbina <annashcherbina@gmail.com>
2017-05-05 09:24:11 -0700

    parsing for gaws

--- anna_code/data_summaries/intersect_subject_list_with_table.py
+++ anna_code/data_summaries/intersect_subject_list_with_table.py
@@ -5,6 +5,8 @@ def parse_args():
     parser.add_argument("--subjects_file")
     parser.add_argument("--table")
     parser.add_argument("--fields_to_ignore",nargs="*")
+    parser.add_argument("--fields_to_use_for_gwas",nargs="*")
+    parser.add_argument("--outf")
     return parser.parse_args()
 
 #handles the case when the header is 1 field shorter than the body of the table 
@@ -36,12 +38,14 @@ def main():
     header=data_table[0].split('\t')  
     field_index=dict()
     field_tally=dict()
+    field_subject_values=dict()
     
     for i in range(len(header)):
         cur_field=header[i]
         if cur_field not in args.fields_to_ignore:
             field_index[cur_field]=i+offset
             field_tally[cur_field]=set([])
+            field_subject_values[cur_field]=dict()
     for row in data_table[1::]:
         tokens=row.split('\t')
         subject=tokens[health_code_col]
@@ -52,9 +56,19 @@ def main():
                 cur_value=tokens[cur_index]
                 if (cur_value!="NA") and (cur_value!=""):
                     field_tally[field].add(subject)
+                    field_subject_values[field][subject]=cur_value 
     #print the table name & field counts
     for field in field_tally:
         print(args.table+":"+field+":"+str(len(field_tally[field])))
+    for field in args.fields_to_use_for_gwas:
+        if field in field_subject_values:
+            outf=open(args.outf+"."+field,'w')
+            outf.write('Subject\t'+field+'\n')
+            for subject in subjects:
+                if subject in field_subject_values[field]:
+                    outf.write(subject+'\t'+str(field_subject_values[field][subject])+'\n')
+                else:
+                    outf.write(subject+'\t'+"NA"+"\n")
         
 if __name__=="__main__":
     main()
