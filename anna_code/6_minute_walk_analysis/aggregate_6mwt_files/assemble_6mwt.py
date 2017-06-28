da3391107cd636e237689226b96aa5101d9da1e5
Anna Shcherbina <annashcherbina@gmail.com>
2017-06-28 15:20:44 -0700

    adding functionality to aggregate by subject or by metric for 6MWT

--- anna_code/6_minute_walk_analysis/aggregate_6mwt_files/assemble_6mwt.py
+++ anna_code/6_minute_walk_analysis/aggregate_6mwt_files/assemble_6mwt.py
@@ -84,7 +84,7 @@ def copy_sources(args):
         blobs=parse_table(args.blob_dir,args.output_dir,source_table,blobs)
     
 
-#create aggregate json w/ all entries for a subject-- this will be a very large file 
+#create aggregate json w/ all entries for a subject
 def group_by_subject(args):
     if not os.path.exists(args.output_dir+"/aggregate_by_subject"):
         os.makedirs(args.output_dir+"/aggregate_by_subject")
@@ -98,6 +98,7 @@ def group_by_subject(args):
                  args.accel_rest_dir,
                  args.device_motion_rest_dir,
                  args.heart_rate_rest_dir]
+    
     metrics=['pedometer_walk_dir',
              'accel_walk_dir',
              'device_motion_walk_dir',
@@ -106,14 +107,73 @@ def group_by_subject(args):
              'device_motion_rest_dir',
              'heart_rate_rest_dir']    
 
-    pedometer_walk_dir_subjects=[f for f in listdir(args.pedometer_walk_dir)]
-    for subject in pedometer_walk_dir_subjects:
-        subject_dict[subject]=['pedometer_walk_dir']
+    for i in range(len(metric_dirs)):
+        cur_metric_dir=metric_dirs[i]
+        cur_metric=metrics[i]
+        print("getting subjects for metric:"+str(cur_metric) )
+        subject_set=[f for f in listdir(cur_metric_dir)]
+        for subject in subject_set:
+            if subject not in subject_dict:
+                subject_dict[subject]=[tuple([cur_metric,cur_metric_dir])]
+            else:
+                subject_dict[subject].append(tuple([cur_metric,cur_metric_dir]))
+    #generate a json for each subject including all the metrics available for that subject
+    #if there are multiple entries for a given metric for a given subject, store them in a list
+    num_subjects=str(len(subject_dict.keys()))
+    subject_index=0 
+    for subject in subject_dict:
+        subject_index+=1
+        if subject_index%100==0:
+            print(subject_index+"/"+num_subjects)
+        subject_dict=dict() 
+        for metric_tuple in subject_dict[subject]:
+            #load the json files
+            cur_metric=metric_tuple[0]
+            cur_metric_dir=metric_tuple[1]
+            subject_dict[cur_metric]=[]
+            subject_metric_files=[f for f in in listdir(cur_metric_dir+'/'+subject)]
+            for f in subject_metric_files:
+                cur_file=open(cur_metric_dir+'/'+subject+f,'r')
+                subject_dict[cur_metric].append(json.load(cur_file))
+        #all metrics for subject have been processed. Save the subject to aggregate directory.
+        outf=open(args.output_dir+"/aggregate_by_subject/"+subject+".json",'w')
+        json.dump(subject_dict,outf)
+        
     
-    
-
+#create aggregate json w/ all subjects' entries for a single metric - this will be a large file!
 def group_by_metric(args):
-    pass 
+    if not os.path.exists(args.output_dir+"/aggregate_by_metric"):
+        os.makedirs(args.output_dir+"/aggregate_by_metric")
+        
+    #create jsons for each metric
+    metric_dirs=[args.pedometer_walk_dir,
+                 args.accel_walk_dir,
+                 args.device_motion_walk_dir,
+                 args.heart_rate_walk_dir,
+                 args.accel_rest_dir,
+                 args.device_motion_rest_dir,
+                 args.heart_rate_rest_dir]
+    
+    metrics=['pedometer_walk_dir',
+             'accel_walk_dir',
+             'device_motion_walk_dir',
+             'heart_rate_walk_dir',
+             'accel_rest_dir',
+             'device_motion_rest_dir',
+             'heart_rate_rest_dir']    
+    for i in range(len(metric_dirs)):
+        cur_metric_dir=metric_dirs[i]
+        cur_metric=metrics[i]
+        print("processing metric:"+str(cur_metric))
+        metric_dict=dict()
+        cur_metric_subjects=[f for f in listdir(cur_metric_dir)]
+        for subject in cur_metric_subjects:
+            metric_dict[subject]=[]
+            subject_metric_files=[f for f in listdir(cur_metric_dir+'/'+subject)]
+            for subject_metric_file in subject_metric_files:
+                cur_file =open(cur_metric_dir+'/'+subject+'/'+subject_metri_file,'r')
+                metric_dict[subject].append(json.load(cur_file))
+
 
 def perform_task(args):
     return{
