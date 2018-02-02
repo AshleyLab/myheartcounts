37c729a9b13c9cadf07d8c7ab85be1bd81aa1af9
Anna Shcherbina <annashcherbina@gmail.com>
2018-02-01 18:29:44 -0800

    download helpers and qc

---
+++ anna_code/download_helpers/find_blob_files_to_download.sh
@@ -0,0 +1,4 @@
+python find_blob_files_to_download.py --table /scratch/PI/euan/projects/mhc/data/tables/cardiovascular-HealthKitWorkoutCollector-v1.tsv  \
+    --columns -1 \
+    --blob_source_dir /scratch/PI/euan/projects/mhc/data/synapseCache \
+    --outf  missing_blobs_cardiovascular-HealthKitWorkoutCollector-v1.tsv
