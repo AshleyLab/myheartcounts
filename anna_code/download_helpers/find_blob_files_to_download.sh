384ef6b2866fc19c0c6225dd14a355346e62d779
Anna Shcherbina <annashcherbina@gmail.com>
2018-02-01 19:03:33 -0800

    qc of all tables with blob data

--- anna_code/download_helpers/find_blob_files_to_download.sh
+++ anna_code/download_helpers/find_blob_files_to_download.sh
@@ -1,4 +1,53 @@
-python find_blob_files_to_download.py --table /scratch/PI/euan/projects/mhc/data/tables/cardiovascular-HealthKitWorkoutCollector-v1.tsv  \
+#python find_blob_files_to_download.py --table /scratch/PI/euan/projects/mhc/data/tables/cardiovascular-HealthKitWorkoutCollector-v1.tsv  \
+#    --columns -1 \
+#    --blob_source_dir /scratch/PI/euan/projects/mhc/data/synapseCache \
+#    --outf  missing_blobs_cardiovascular-HealthKitWorkoutCollector-v1.tsv
+
+python find_blob_files_to_download.py --table /scratch/PI/euan/projects/mhc/data/tables/cardiovascular-motionTracker-v1.tsv  \
     --columns -1 \
     --blob_source_dir /scratch/PI/euan/projects/mhc/data/synapseCache \
-    --outf  missing_blobs_cardiovascular-HealthKitWorkoutCollector-v1.tsv
+    --outf  missing_blobs_cardiovascular-motionTracker-v1.tsv
+
+
+python find_blob_files_to_download.py --table /scratch/PI/euan/projects/mhc/data/tables/cardiovascular-motionActivityCollector-v1.tsv  \
+    --columns -1 \
+    --blob_source_dir /scratch/PI/euan/projects/mhc/data/synapseCache \
+    --outf  missing_blobs_cardiovascular-motionActivityCollector-v1.tsv
+
+python find_blob_files_to_download.py --table /scratch/PI/euan/projects/mhc/data/tables/cardiovascular-HealthKitDataCollector-v1.tsv  \
+    --columns -1 \
+    --blob_source_dir /scratch/PI/euan/projects/mhc/data/synapseCache \
+    --outf  missing_blobs_cardiovascular-HealthKitDataCollector-v1.tsv
+
+python find_blob_files_to_download.py --table /scratch/PI/euan/projects/mhc/data/tables/cardiovascular-HealthKitSleepCollector-v1.tsv  \
+    --columns -1 \
+    --blob_source_dir /scratch/PI/euan/projects/mhc/data/synapseCache \
+    --outf  missing_blobs_cardiovascular-HealthKitSleepCollector-v1.tsv
+
+
+python find_blob_files_to_download.py --table /scratch/PI/euan/projects/mhc/data/tables/cardiovascular-6-Minute\ Walk\ Test_SchemaV4-v1.tsv   \
+    --columns -1 -2 -3 -4 -5 -6 -7 \
+    --blob_source_dir /scratch/PI/euan/projects/mhc/data/synapseCache \
+    --outf  missing_blobs_cardiovascular-6-Minute_Walk_Test_SchemaV4-v1.tsv 
+
+
+python find_blob_files_to_download.py --table /scratch/PI/euan/projects/mhc/data/tables/cardiovascular-6MinuteWalkTest-v2.tsv \
+    --columns -1 -2 -3 -4 -5 -6 -7 \
+    --blob_source_dir /scratch/PI/euan/projects/mhc/data/synapseCache \
+    --outf  missing_blobs_cardiovascular-6MinuteWalkTest-v2.tsv
+
+
+python find_blob_files_to_download.py --table /scratch/PI/euan/projects/mhc/data/tables/cardiovascular-6MWT\ Displacement\ Data-v1.tsv  \
+    --columns -1 -2 -3 -4 \
+    --blob_source_dir /scratch/PI/euan/projects/mhc/data/synapseCache \
+    --outf  missing_blobscardiovascular-6MWT_Displacement_Data-v1.tsv
+
+python find_blob_files_to_download.py --table /scratch/PI/euan/projects/mhc/data/tables/cardiovascular-6MWT\ Displacement\ Data-v1.tsv  \
+    --columns -1 -2 -3 -4 \
+    --blob_source_dir /scratch/PI/euan/projects/mhc/data/synapseCache \
+    --outf  missing_blobscardiovascular-6MWT_Displacement_Data-v1.tsv
+
+
+
+
+
