36770b17626ae3aad7a2282b5533a3d86571cf76
Anna Shcherbina <annashcherbina@gmail.com>
2018-03-14 20:44:05 -0700

    code to cleanup common problems with healthkit file formatting

---
+++ anna_code/data_cleanup_and_formatting/cleanup_problematic_healthkit_tables.py
@@ -0,0 +1,42 @@
+#cleans up common errors when parsing healthkit files 
+import argparse 
+import re
+def parse_args():  
+    parser=argparse.ArgumentParser(description="clean up common errors when parsing healthkit files")
+    parser.add_argument("file_list")
+    return parser.parse_args() 
+
+def main(): 
+    args=parse_args() 
+    files=open(args.file_list,'r').read().strip().split('\n')
+    for f in files: 
+        data=open(f,'r').read().strip().split('\n')
+        outf=open(f+'.filtered','w') 
+        header="startTime,endTime,type,value,unit,source,sourceIdentifier"
+        first_line=data[0] 
+        #1. make sure there is a header 
+        if not(first_line.startswith("startTime")): 
+            outf.write(header+'\n') 
+            outf.write(first_line+'\n') 
+        else: 
+            outf.write(first_line+'\n')
+        if len(data)>1: 
+            for line in data[1::]: 
+                tokens=line.split(',') 
+                #2. make sure there are no stray header fields 
+                if tokens[0].startswith('startTime'): 
+                    continue 
+                #3. make sure all lines contain appropriate number of tokens 
+                if len(tokens)!=7: 
+                    continue 
+                #4. remove non-alpha-numeric characters from last two tokens  
+                tokens[5]=re.sub(r'\W+','',tokens[5])
+                tokens[6]=re.sub(r'\W+','',tokens[6])
+                outf.write(','.join(tokens)+'\n')
+
+
+
+
+
+if __name__=="__main__": 
+    main() 
