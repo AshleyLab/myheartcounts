093b67e8a3c7dd8441f73fbb26149c6bfff5e8df
Anna Shcherbina <annashcherbina@gmail.com>
2018-06-15 15:41:31 -0700

    geographic distribution of MHC users who provided 23andMe data

---
+++ anna_code/23andme/geographic_distribution/plot_23andme_users.R
@@ -0,0 +1,18 @@
+rm(list=ls())
+library(ggplot2)
+library(fiftystater)
+data("fifty_states")
+data=read.table("us.tally",header=TRUE,sep='\t')
+p=ggplot(data,aes(map_id=State))+
+  geom_map(aes(fill=Users),map=fifty_states)+
+  expand_limits(x=fifty_states$long,y=fifty_states$lat)+
+  coord_map()+
+  scale_x_continuous(breaks = NULL) + 
+  scale_y_continuous(breaks = NULL) +
+  labs(x = "", y = "") +
+  theme(legend.position = "bottom", 
+        panel.background = element_blank())
+
+#p
+# add border boxes to AK/HI
+p + fifty_states_inset_boxes() 
