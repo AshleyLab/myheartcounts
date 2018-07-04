079636705cb20cd3e566b08ac99d3d9681828100
Anna Shcherbina <annashcherbina@gmail.com>
2018-07-04 11:23:06 -0700

    demographics summary add startt time and end time

--- anna_code/demographics/summarize_demographics.sh
+++ anna_code/demographics/summarize_demographics.sh
@@ -1,5 +1,6 @@
-python summarize_demographics.py --summary_tables /scratch/PI/euan/projects/mhc/data/tables/cardiovascular-2-APHHeartAge-7259AC18-D711-47A6-ADBD-6CFCECDED1DF-v1.tsv /scratch/PI/euan/projects/mhc/data/tables/cardiovascular-2-APHHeartAge-7259AC18-D711-47A6-ADBD-6CFCECDED1DF-v2.tsv /scratch/PI/euan/projects/mhc/data/tables/cardiovascular-heart_risk_and_age-v1.tsv /scratch/PI/euan/projects/mhc/data/tables/cardiovascular-NonIdentifiableDemographicsTask-v2.tsv  \
-       --outf demographics_summary_v1_v2.tsv
+#python summarize_demographics.py --summary_tables /scratch/PI/euan/projects/mhc/data/tables/cardiovascular-2-APHHeartAge-7259AC18-D711-47A6-ADBD-6CFCECDED1DF-v1.tsv /scratch/PI/euan/projects/mhc/data/tables/cardiovascular-2-APHHeartAge-7259AC18-D711-47A6-ADBD-6CFCECDED1DF-v2.tsv /scratch/PI/euan/projects/mhc/data/tables/cardiovascular-heart_risk_and_age-v1.tsv /scratch/PI/euan/projects/mhc/data/tables/cardiovascular-NonIdentifiableDemographicsTask-v2.tsv \
+#    --outf demographics_summary_v1.tsv \
+#    --end_date 2016-10-02
 
-
-
+python summarize_demographics.py --summary_tables /scratch/PI/euan/projects/mhc/data/tables/cardiovascular-2-APHHeartAge-7259AC18-D711-47A6-ADBD-6CFCECDED1DF-v1.tsv /scratch/PI/euan/projects/mhc/data/tables/cardiovascular-2-APHHeartAge-7259AC18-D711-47A6-ADBD-6CFCECDED1DF-v2.tsv /scratch/PI/euan/projects/mhc/data/tables/cardiovascular-heart_risk_and_age-v1.tsv /scratch/PI/euan/projects/mhc/data/tables/cardiovascular-NonIdentifiableDemographicsTask-v2.tsv \
+    --outf demographics_summary_v1_v2.tsv
