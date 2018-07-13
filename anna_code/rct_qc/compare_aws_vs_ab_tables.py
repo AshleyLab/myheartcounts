a66b6a156c4b4433216a6307ed231348f3fca0bf
Anna Shcherbina <annashcherbina@gmail.com>
2018-07-13 13:52:13 -0700

    added appVersion for each healthCode/createdOn entry

--- anna_code/rct_qc/compare_aws_vs_ab_tables.py
+++ anna_code/rct_qc/compare_aws_vs_ab_tables.py
@@ -13,6 +13,7 @@ aws_data=p.load(open("aws_interventions.p",'rb'))
 ab_data=pd.read_csv("cardiovascular-ABTestResults-v1.tsv",header=0,sep='\t')
 steps=pd.read_csv('healthkit_combined.stepcount.txt',header=0,sep='\t')
 distance=pd.read_csv('healthkit_combined.distance.txt',header=0,sep='\t')
+appversion=pd.read_csv("appVersionWithDates.tsv",header=None,sep='\t')
 print("loaded tables of interest") 
 
 #use a dictionary to keep track of AB-test values,AWS values, HealthKit steps, HealthKit distance 
@@ -24,21 +25,20 @@ for index,row in ab_data.iterrows():
     if health_code not in subject_dict: 
         subject_dict[health_code]=dict() 
     created_on=parse(row['createdOn']).date() 
-
-    #count 7 days back from the created_on date to determine start of baseline week
-    for d in range(1,baseline_days+1): 
-        baseline_date=created_on-timedelta(days=d)
+    days_in_study=row['ABTestResult.days_in_study']
+    
+    #the days in range(1,days_in_study) before created_on date constitute the baseline week of data 
+    first_baseline=created_on-timedelta(days=days_in_study)
+    for d in range(1,days_in_study+1):
+        baseline_date=create_on-timedelta(days=d)         
         if baseline_date not in subject_dict[health_code]: 
             subject_dict[health_code][baseline_date]=dict() 
         subject_dict[health_code][baseline_date]['ABTest']='baseline'
- 
             
     #assign 7 days to each of the 4 interventions, breaking out of the loops
     # once days_in_study days have been accounted for 
     interventions=row['ABTestResult.variable_value'].split(',')
-    days_in_study=row['ABTestResult.days_in_study']
-    final_date=created_on+timedelta(days=days_in_study)
-
+    final_date=created_on+timedelta(days=len(interventions)*intervention_days)
     cur_date=created_on
     for intervention in interventions: 
         days_in_cur_intervention=0
@@ -107,11 +107,26 @@ for index,row in distance.iterrows():
     subject_dict[subject][cur_date]['dist'].append('\t'.join([str(i) for i in [dist_count,dist_source,dist_blobs]]))
 
 print("Assigned HealthKit Distance to all subjects") 
+print("Determining appVersion for each healthCode/createOn")
+for index,row in appversion.iterrows(): 
+    subject=row['healthCode'] 
+    cur_date=parse(row['createdOn']).date() 
+    cur_appVersion=row['appVersion'] 
+    if subject in subject_dict: 
+        if cur_date in subject_dict[subject]: 
+            subject_dict[subject][cur_date]['appVersion']=cur_appVersion 
+
+
 print("Writing output file") 
 outf=open("AWS_vs_ABTable_HealthKit_Steps_Distance.tsv",'w')
 outf.write('\t'.join(['Subject','Date','ABTest','AWS','Metric','Value','Source','Blobs'])+'\n')
 for subject in subject_dict: 
-    for cur_date in subject_dict[subject]: 
+    for cur_date in subject_dict[subject]:
+        if 'appVersion'] in subject_dict[subject][cur_date]:
+            cur_appVersion=subject_dict[subject][cur_date]['appVersion']
+        else: 
+            cur_appVersion="NONE"
+
         if 'ABTest' in subject_dict[subject][cur_date]: 
             cur_abtest=subject_dict[subject][cur_date]['ABTest']
         else: 
@@ -125,16 +140,16 @@ for subject in subject_dict:
         if 'steps' in subject_dict[subject][cur_date]: 
             for step_entry in subject_dict[subject][cur_date]['steps']: 
                 try:
-                    outf.write('\t'.join([subject,str(cur_date),cur_abtest,cur_aws,'HealthKitSteps',step_entry])+'\n')
+                    outf.write('\t'.join([subject,str(cur_date),cur_appVersion,cur_abtest,cur_aws,'HealthKitSteps',step_entry])+'\n')
                 except: 
                     pdb.set_trace() 
         else: 
-            outf.write('\t'.join([subject,str(cur_date),cur_abtest,cur_aws,'HealthKitSteps',"NONE"])+'\n')
+            outf.write('\t'.join([subject,str(cur_date),cur_appVersion,cur_abtest,cur_aws,'HealthKitSteps',"NONE"])+'\n')
 
         if 'dist' in subject_dict[subject][cur_date]:
             for dist_entry in subject_dict[subject][cur_date]['dist']: 
-                outf.write('\t'.join([subject,str(cur_date),cur_abtest,cur_aws,'HealthKitDistance',dist_entry])+'\n')
+                outf.write('\t'.join([subject,str(cur_date),cur_appVersion,cur_abtest,cur_aws,'HealthKitDistance',dist_entry])+'\n')
         else:
-            outf.write('\t'.join([subject,str(cur_date),cur_abtest,cur_aws,'HealthKitDistance',"NONE"])+'\n')
+            outf.write('\t'.join([subject,str(cur_date),cur_appVersion,cur_abtest,cur_aws,'HealthKitDistance',"NONE"])+'\n')
 
     
