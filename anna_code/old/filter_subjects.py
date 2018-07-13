c6cf7362c0af7c2922ed666c9bcabf2ec6912938
Anna Shcherbina <annashcherbina@gmail.com>
2018-07-13 13:43:17 -0700

    generated an appVersion/healthCode/createdOn master table

--- anna_code/old/filter_subjects.py
+++
@@ -1,7 +0,0 @@
-data=open('../subjects_sorted_unique.txt','r').read().split('\n') 
-if '' in data: 
-    data.remove('') 
-outf=open('../subjects_sorted_unique.filtered','w') 
-for line in data: 
-    if (len(line)==36) and (line.count('-')==4): 
-        outf.write(line+'\n')
