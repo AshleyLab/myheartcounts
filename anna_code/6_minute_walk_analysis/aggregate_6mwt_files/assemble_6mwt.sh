67f18e13d5752a6905fbdea6af45ad77a3723221
Anna Shcherbina <annashcherbina@gmail.com>
2018-07-13 16:45:05 -0700

    in aggregation of 6mwt files, renamed the files by their corresponding blob id's; added 2 new source tables for 6mwt data

--- anna_code/6_minute_walk_analysis/aggregate_6mwt_files/assemble_6mwt.sh
+++ anna_code/6_minute_walk_analysis/aggregate_6mwt_files/assemble_6mwt.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 #copy the files to common location 
-#python assemble_6mwt.py --source_tables /scratch/PI/euan/projects/mhc/data/tables/6mwt/cardiovascular-6-MinuteWalkTest_SchemaV4-v1.tsv /scratch/PI/euan/projects/mhc/data/tables/6mwt/cardiovascular-6MinuteWalkTest-v2_withSteps.tsv /scratch/PI/euan/projects/mhc/data/tables/6mwt/cardiovascular-6MinuteWalkTest-v2_withSteps_filtered.tsv /scratch/PI/euan/projects/mhc/data/tables/6mwt/cardiovascular-6MinuteWalkTest-v2.tsv --task copy_sources --output_dir /scratch/PI/euan/projects/mhc/data/6mwt --blob_dir /scratch/PI/euan/projects/mhc/data/synapseCache --healthCode_indices 5 1 1 2 --resume_from 0
+python assemble_6mwt.py --source_tables \
+    /scratch/PI/euan/projects/mhc/data/tables/cardiovascular-6-Minute\ Walk\ Test_SchemaV4-v1.tsv \
+    /scratch/PI/euan/projects/mhc/data/tables/cardiovascular-6MinuteWalkTest-v2.tsv \
+    /scratch/PI/euan/projects/mhc/data/tables/6-Minute\ Walk\ Test_SchemaV4-v2.tsv  \
+    /scratch/PI/euan/projects/mhc/data/tables/6-Minute\ Walk\ Test_SchemaV4-v6.tsv \
+    --task copy_sources \
+    --output_dir /scratch/PI/euan/projects/mhc/data/6mwt_tmp \
+    --blob_dir /scratch/PI/euan/projects/mhc/data/synapseCache \
+    --healthCode_indices 5 2 5 5 \
+    --resume_from 0
 
-#aggregate by subject 
-#python assemble_6mwt.py --source_tables /scratch/PI/euan/projects/mhc/data/tables/6mwt/cardiovascular-6-MinuteWalkTest_SchemaV4-v1.tsv /scratch/PI/euan/projects/mhc/data/tables/6mwt/cardiovascular-6MinuteWalkTest-v2_withSteps.tsv /scratch/PI/euan/projects/mhc/data/tables/6mwt/cardiovascular-6MinuteWalkTest-v2_withSteps_filtered.tsv /scratch/PI/euan/projects/mhc/data/tables/6mwt/cardiovascular-6MinuteWalkTest-v2.tsv --task copy_sources --output_dir /scratch/PI/euan/projects/mhc/data/6mwt --blob_dir /scratch/PI/euan/projects/mhc/data/synapseCache --healthCode_indices 5 1 1 2 --resume_from 0  
+
+
 
 #aggregate by subject
-python assemble_6mwt.py --task group_by_subject\
-       --pedometer_walk_dir /scratch/PI/euan/projects/mhc/data/6mwt/pedometer_walk_dir\
-       --accel_walk_dir /scratch/PI/euan/projects/mhc/data/6mwt/accel_walk_dir\
-       --device_motion_walk_dir /scratch/PI/euan/projects/mhc/data/6mwt/device_motion_walk_dir\
-       --heart_rate_walk_dir /scratch/PI/euan/projects/mhc/data/6mwt/heart_rate_walk_dir\
-       --accel_rest_dir /scratch/PI/euan/projects/mhc/data/6mwt/accel_rest_dir\
-       --device_motion_rest_dir /scratch/PI/euan/projects/mhc/data/6mwt/device_motion_rest_dir\
-       --heart_rate_rest_dir /scratch/PI/euan/projects/mhc/data/6mwt/heart_rate_rest_dir\
-       --output_dir /scratch/PI/euan/projects/mhc/data/6mwt\
-       --subject_index $1
+#python assemble_6mwt.py --task group_by_subject \
+#       --pedometer_walk_dir /scratch/PI/euan/projects/mhc/data/6mwt_tmp/pedometer_walk_dir \
+#       --accel_walk_dir /scratch/PI/euan/projects/mhc/data/6mwt_tmp/accel_walk_dir \
+#       --device_motion_walk_dir /scratch/PI/euan/projects/mhc/data/6mwt_tmp/device_motion_walk_dir \
+#       --heart_rate_walk_dir /scratch/PI/euan/projects/mhc/data/6mwt_tmp/heart_rate_walk_dir \
+#       --accel_rest_dir /scratch/PI/euan/projects/mhc/data/6mwt_tmp/accel_rest_dir \
+#       --device_motion_rest_dir /scratch/PI/euan/projects/mhc/data/6mwt_tmp/device_motion_rest_dir \
+#       --heart_rate_rest_dir /scratch/PI/euan/projects/mhc/data/6mwt_tmp/heart_rate_rest_dir \
+#       --output_dir /scratch/PI/euan/projects/mhc/data/6mwt_tmp \
+#       --subject_index $1
 
