5aa1c9083106f99b8796c7699311dc5e337cbfb1
Anna Shcherbina <annashcherbina@gmail.com>
2018-07-13 14:49:40 -0700

    streamlined ABTest and AWS interventions for data parsing, added appVersion for each subject/date

--- anna_code/motion_tracker_analysis/v2/aggregators.py
+++ anna_code/motion_tracker_analysis/v2/aggregators.py
@@ -1,60 +1,32 @@
 from datetime import datetime,timedelta
 from dateutil.parser import parse
 import pdb 
-#assign day index to applied intervention
-def get_day_index_to_intervention(interventions):
-    day_index_to_intervention=dict()
-    for day_index in range(7):
-        day_index_to_intervention[day_index]="None"
-    for day_index in range(7,14):
-        day_index_to_intervention[day_index]=interventions[0]
-    for day_index in range(14,21):
-        day_index_to_intervention[day_index]=interventions[1]
-    for day_index in range(21,28):
-        day_index_to_intervention[day_index]=interventions[2]
-    for day_index in range(28,35):
-        day_index_to_intervention[day_index]=interventions[3]
-    return day_index_to_intervention
 
-#label each day as a weekend or weekday 
-def get_weekday_or_weekend_label(days,first_day):
-    index_to_type={}
-    for i in range(len(days)):
-        cur_day=days[i]
-        cur_index=(cur_day-first_day).days
-        index_to_type[cur_index]=cur_day.weekday()
-    return index_to_type
 
-def get_intervention(intervention_order,subject,day):
-    if subject in intervention_order: 
-        first_intervention_day=min(intervention_order[subject].keys())
-        last_intervention_day=max(intervention_order[subject].keys())
 
-        if day in intervention_order[subject]: 
-            return list(intervention_order[subject][day])[0] 
-        elif (day < first_intervention_day): #check if this is baseline data 
-            return "Baseline"
-        elif (day > last_intervention_day) : #check if this is post-intervention data 
-            return "PostIntervention"
-        else: 
-            return "InterventionGap" 
-    return "NA" 
-
-def aggregate_motion_tracker_aws(subject_daily_vals,days_in_study,intervention_order,outf_prefix): 
+def aggregate_motion_tracker(subject_daily_vals,interventions,outf_prefix): 
     duration_vals=subject_daily_vals[0]
     fraction_vals=subject_daily_vals[1]
     numentries_vals=subject_daily_vals[2]
     
     outf=open(outf_prefix,'w')
-    outf.write('Subject\tDaysInStudy\tIntervention\tDate\tActivity\tDuration_in_Minutes\tFraction\tNumentries\tSourceBlobs\n')
+    outf.write('Subject\tDate\tWeekday\tappVersion\tABTest\tAWS\tActivity\tDuration_in_Minutes\tFraction\tNumentries\tSourceBlobs\n')
     for subject in duration_vals: 
-        subject_days_in_study=0 
-        if subject in days_in_study: 
-            subject_days_in_study=days_in_study[subject]
         for day in duration_vals[subject]: 
+            cur_weekday=day.weekday() 
+            try:
+                cur_appversion=interventions[subject][day]['appVersion'] 
+            except: 
+                cur_appversion="NONE"
+            try:
+                cur_abtest=interventions[subject][day]['ABTest'] 
+            except: 
+                cur_abtest="NONE" 
+            try:
+                cur_aws=interventions[subject][day]['AWS'] 
+            except: 
+                cur_aws="NONE"            
             numentries=numentries_vals[subject][day]
-            #determine the intervention that is applied for this subject on this day 
-            cur_intervention=get_intervention(intervention_order,subject,day)
             for activity in duration_vals[subject][day]: 
                 #sum durations across blobs 
                 blobs=','.join([str(i) for i in duration_vals[subject][day][activity].keys()])
@@ -64,140 +36,48 @@ def aggregate_motion_tracker_aws(subject_daily_vals,days_in_study,intervention_o
                 if activity in fraction_vals[subject][day]: 
                     fraction=sum([i for i in fraction_vals[subject][day][activity].values()])
                 outf.write(subject+'\t'+
-                           str(subject_days_in_study)+'\t'+
-                           cur_intervention+'\t'+
                            str(day)+'\t'+
+                           str(cur_weekday)+'\t'+
+                           str(cur_abtest)+'\t'+
+                           str(cur_aws)+'\t'+
                            activity+'\t'+
                            str(duration)+'\t'+
                            str(fraction)+'\t'+
                            str(numentries)+'\t'+
                            str(blobs)+'\n')
                 
-def aggregate_motion_tracker_synapse(subject_daily_vals,days_in_study,intervention_order,outf_prefix):
-    duration_vals=subject_daily_vals[0]
-    fraction_vals=subject_daily_vals[1]
-    numentries=subject_daily_vals[2]
-    
+                    
+def aggregate_healthkit_data_collector(subject_daily_vals,interventions,outf_prefix):
     outf=open(outf_prefix,'w')
-    outf.write('Subject\tDaysInStudy\tIntervention\tDayIndex\tDayType\tActivity\tDuration_in_Minutes\tFraction\tNumentries\n')
-    for subject in duration_vals:
-        try:
-            cur_days_in_study=days_in_study[subject]
-        except:
-            cur_days_in_study='NA'
-        try:
-            interventions=intervention_order[subject]
-        except:
-            interventions=["NA"]*4
-        #map day index to the applied intervention 
-        day_index_to_intervention=get_day_index_to_intervention(interventions)
-        cur_duration_vals=duration_vals[subject]
-        cur_fraction_vals=fraction_vals[subject]
-        cursubject_numentries=numentries[subject]
-        #sort the days!
-        subject_days=sorted(cur_duration_vals.keys())
-        if len(subject_days)==0:
-            continue 
-        first_day=subject_days[0]
-        weekday_or_weekend=get_weekday_or_weekend_label(subject_days,first_day)
-        for day in subject_days:
-            day_index=(day - first_day).days
-            cursubject_day_numentries=cursubject_numentries[day] 
-            #handle the case when the subject has been in the study longer than 35 days 
-            if day_index not in day_index_to_intervention:
-                cur_intervention=interventions[-1]
-            else:
-                cur_intervention=day_index_to_intervention[day_index]
-            cur_weekday_or_weekend=weekday_or_weekend[day_index]
-            for activity in cur_duration_vals[day]:
-                cur_activity_duration=cur_duration_vals[day][activity].total_seconds()/60.0
-                if activity in cur_fraction_vals[day]:
-                    cur_activity_fraction=cur_fraction_vals[day][activity]
-                else:
-                    cur_activity_fraction=0
-                outf.write(subject+\
-                           '\t'+str(cur_days_in_study)+\
-                           '\t'+str(cur_intervention)+\
-                           '\t'+str(day_index)+\
-                           '\t'+str(cur_weekday_or_weekend)+\
-                           '\t'+activity+\
-                           '\t'+str(round(cur_activity_duration,3))+\
-                           '\t'+str(round(cur_activity_fraction,3))+\
-                           '\t'+str(cursubject_day_numentries)+\
-                           '\n')
-            
-        
-def aggregate_healthkit_data_collector_aws(subject_daily_vals,days_in_study,intervention_order,outf_prefix):
-    outf=open(outf_prefix,'w') 
-    outf.write("Subject\tDaysInStudy\tIntervention\tDate\tMetric\tValue\tSource\tSourceBlobs\n")
+    outf.write("Subject\tDate\tWeekDay\tappVersion\tABTest\tAWS\tMetric\tValue\tSource\tSourceBlobs\n")
     for subject in subject_daily_vals: 
-        subject_days_in_study=0 
-        if subject in days_in_study: 
-            subject_days_in_study=days_in_study[subject]
-        for day in subject_daily_vals[subject]:
-            cur_intervention=get_intervention(intervention_order,subject,day)
+        for day in subject_daily_vals[subject]: 
+            cur_weekday=day.weekday() 
+            try:
+                cur_appversion=interventions[subject][day]['appVersion'] 
+            except: 
+                cur_appversion="NONE"
+            try:
+                cur_abtest=interventions[subject][day]['ABTest'] 
+            except: 
+                cur_abtest="NONE" 
+            try:
+                cur_aws=interventions[subject][day]['AWS'] 
+            except: 
+                cur_aws="NONE"
             for metric in subject_daily_vals[subject][day]: 
                 for source in subject_daily_vals[subject][day][metric]: 
                     blobs=','.join([str(i) for i in subject_daily_vals[subject][day][metric][source].keys()])
                     value=sum([i for i in subject_daily_vals[subject][day][metric][source].values()])
                     outf.write(subject+'\t'+
-                               str(subject_days_in_study)+'\t'+
-                               str(cur_intervention)+'\t'+
                                str(day)+'\t'+
+                               str(cur_weekday)+'\t'+
+                               str(cur_appversion)+'\t'+
+                               str(cur_abtest)+'\t'+
+                               str(cur_aws)+'\t'+
                                str(metric)+'\t'+
                                str(value)+'\t'+
                                str(source)+'\t'+
                                str(blobs)+'\n')
-                               
-                    
-def aggregate_healthkit_data_collector_synapse(subject_daily_vals,days_in_study,intervention_order,outf_prefix):
-    outf=open(outf_prefix,'w')
-    outf.write('Subject\tDaysInStudy\tIntervention\tDayIndex\tDayType\tMetric\tValue\tSource\n')
-    for subject in subject_daily_vals:
-        try:
-            cur_days_in_study=days_in_study[subject]
-        except:
-            cur_days_in_study='NA'
-        try:
-            interventions=intervention_order[subject]
-        except:
-            interventions=["NA"]*4
 
-        #map day index to the applied intervention 
-        day_index_to_intervention=get_day_index_to_intervention(interventions)
-        cur_subject_daily_vals=subject_daily_vals[subject]
 
-        #sort the days!
-        subject_days=sorted(cur_subject_daily_vals.keys())
-        if len(subject_days)==0:
-            continue 
-        first_day=subject_days[0]
-        weekday_or_weekend=get_weekday_or_weekend_label(subject_days,first_day)
-        for day in subject_days:
-            day_index=(day - first_day).days
-            
-            #handle the case when the subject has been in the study longer than 35 days 
-            if day_index not in day_index_to_intervention:
-                cur_intervention=interventions[-1]
-            else:
-                cur_intervention=day_index_to_intervention[day_index]
-            cur_weekday_or_weekend=weekday_or_weekend[day_index]
-            for datatype in cur_subject_daily_vals[day]:
-                for source_tuple in cur_subject_daily_vals[day][datatype]: 
-                    cur_value=cur_subject_daily_vals[day][datatype][source_tuple]
-                    try:
-                        outf.write(subject+\
-                                   '\t'+str(cur_days_in_study)+\
-                                   '\t'+str(cur_intervention)+\
-                                   '\t'+str(day_index)+\
-                                   '\t'+str(cur_weekday_or_weekend)+\
-                                   '\t'+datatype.decode("utf-8")+\
-                                   '\t'+str(round(cur_value,3))+\
-                                   '\t'+str(source_tuple)+\
-                                   '\n')
-                    except:
-                        print("error in writing to output file, probably the utf-8 encoding thing")
-                        pdb.set_trace()
-                        
-                        
-    
