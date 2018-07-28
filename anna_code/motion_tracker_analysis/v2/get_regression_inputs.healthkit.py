f4f0ef2970be0e4fed5415187d09acf3d25e2b05
Anna Shcherbina <annashcherbina@gmail.com>
2018-07-28 15:34:09 -0700

    prepping for rct analysis

--- anna_code/motion_tracker_analysis/v2/get_regression_inputs.healthkit.py
+++ anna_code/motion_tracker_analysis/v2/get_regression_inputs.healthkit.py
@@ -5,42 +5,39 @@ import pdb
 import sys 
 data=open(sys.argv[1],'r').read().strip().split('\n') 
 outf=open(sys.argv[1]+'.regression','w')
+
+#get days when coaching began 
+subject_to_start_date=dict() 
+start_dates=open("/scratch/PI/euan/projects/mhc/data/tables/cardiovascular-ABTestResults-v1.tsv",'r').read().strip().split('\n') 
+for line in start_dates[1::]: 
+    tokens=line.split('\t') 
+    subject=tokens[5] 
+    created_on=datetime.datetime.strptime(tokens[8],'%Y-%m-%d %H:%M:%S').date() 
+    if subject not in subject_to_start_date: 
+        subject_to_start_date[subject]=[created_on]
+    else: 
+        subject_to_start_date[subject].append(created_on) 
 header=data[0] 
 outf.write(header+'\t'+'dayIndex'+'\t'+'WatchVsPhone'+'\n')
-subject_dict=dict() 
 valid_interventions=['baseline','cluster','read_aha','stand','walk']
-
 valid_interventions_aws=['APHClusterModule','APHReadAHAWebsiteModule','APHStandModule','APHWalkModule']
-for line in data[1::]: 
-    tokens=line.split('\t') 
-    #USE ABTEST SCHEDULED INTERVENTION
-    intervention=tokens[4] 
-    if intervention=="NA": 
-        continue 
-    subject=tokens[0] 
-    day=datetime.datetime.strptime(tokens[1],'%Y-%m-%d').date() 
-    #keep track of valid interventions 
-    if intervention in valid_interventions: 
-        if subject not in subject_dict: 
-            subject_dict[subject]=dict() 
-            subject_dict[subject]['first']=day
-            subject_dict[subject]['last']=day
-        elif (day > subject_dict[subject]['last']): 
-            subject_dict[subject]['last']=day 
-        elif (day < subject_dict[subject]['first']): 
-            subject_dict[subject]['first']=day 
-print("done w/ first pass") 
+
 for line in data[1::]: 
     tokens=line.split('\t') 
     intervention=tokens[4] 
-    if intervention=="NA": 
+    if intervention not in valid_interventions: 
         continue 
     subject=tokens[0] 
-    if subject not in subject_dict: 
-        continue 
     day=datetime.datetime.strptime(tokens[1],'%Y-%m-%d').date() 
-    print(intervention) 
-    day_index=(day-subject_dict[subject]['first']).days 
+    #find the day index
+    if subject not in subject_to_start_date: 
+        continue 
+    deltas=[(day-i).days for i in subject_to_start_date[subject]]
+    min_delta=deltas[0] 
+    for d in deltas: 
+        if abs(d) < abs(min_delta): 
+            min_delta=d
+    day_index=min_delta 
     if tokens[8].lower().__contains__('phone'): 
         device_type='phone'
     elif tokens[8].lower().__contains__('watch'):
--- anna_code/motion_tracker_analysis/v2/get_regression_inputs.motiontracker.py
+++ anna_code/motion_tracker_analysis/v2/get_regression_inputs.motiontracker.py
@@ -5,46 +5,39 @@ import pdb
 import sys 
 data=open(sys.argv[1],'r').read().strip().split('\n') 
 outf=open(sys.argv[1]+'.regression','w')
+#get days when coaching began 
+subject_to_start_date=dict() 
+start_dates=open("/scratch/PI/euan/projects/mhc/data/tables/cardiovascular-ABTestResults-v1.tsv",'r').read().strip().split('\n') 
+for line in start_dates[1::]: 
+    tokens=line.split('\t') 
+    subject=tokens[5] 
+    created_on=datetime.datetime.strptime(tokens[8],'%Y-%m-%d %H:%M:%S').date() 
+    if subject not in subject_to_start_date: 
+        subject_to_start_date[subject]=[created_on]
+    else: 
+        subject_to_start_date[subject].append(created_on) 
+#pdb.set_trace() 
 header=data[0] 
-outf.write(header+'\t'+'dayIndex'+'\n')
-subject_dict=dict() 
-valid_interventions=['APHClusterModule','APHReadAHAWebsiteModule','APHStandModule','APHWalkModule']
+outf.write(header+'\t'+'dayIndex'+'\t'+'WatchVsPhone'+'\n')
+valid_interventions=['baseline','cluster','read_aha','stand','walk']
+valid_interventions_aws=['APHClusterModule','APHReadAHAWebsiteModule','APHStandModule','APHWalkModule']
+
 for line in data[1::]: 
     tokens=line.split('\t') 
-    intervention=tokens[2] 
-    if intervention=="NA": 
+    intervention=tokens[3] 
+    #print(intervention)
+    if intervention not in valid_interventions: 
         continue 
     subject=tokens[0] 
-    day=datetime.datetime.strptime(tokens[3],'%Y-%m-%d').date() 
-    #keep track of valid interventions 
-    if intervention in valid_interventions: 
-        if subject not in subject_dict: 
-            subject_dict[subject]=dict() 
-            subject_dict[subject]['first']=day
-            subject_dict[subject]['last']=day
-        elif (day > subject_dict[subject]['last']): 
-            subject_dict[subject]['last']=day 
-        elif (day < subject_dict[subject]['first']): 
-            subject_dict[subject]['first']=day 
-print("done w/ first pass") 
-for line in data[1::]: 
-    tokens=line.split('\t') 
-    intervention=tokens[2] 
-    if intervention=="NA": 
+    day=datetime.datetime.strptime(tokens[1],'%Y-%m-%d').date() 
+    #find the day index
+    if subject not in subject_to_start_date: 
         continue 
-    subject=tokens[0] 
-    if subject not in subject_dict: 
-        continue 
-    day=datetime.datetime.strptime(tokens[3],'%Y-%m-%d').date() 
-    day_index=(day-subject_dict[subject]['first']).days 
-    if (day_index <-8): 
-        continue 
-        #we only look at 1 week post final intervention 
-    if (day_index > 50): 
-            continue 
-    outf.write(line+'\t'+str(day_index)+'\n')
-
-        
-        
-        
-
+    deltas=[(day-i).days for i in subject_to_start_date[subject]]
+    min_delta=deltas[0] 
+    for d in deltas: 
+        if abs(d) < abs(min_delta): 
+            min_delta=d
+    day_index=min_delta 
+    device_type='phone'
+    outf.write(line+'\t'+str(day_index)+'\t'+device_type+'\n')
