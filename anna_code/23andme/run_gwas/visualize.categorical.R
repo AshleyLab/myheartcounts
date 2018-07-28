92182193eb1875b6470e071ec37c407b09688a2b
Anna Shcherbina <annashcherbina@gmail.com>
2018-07-28 15:40:40 -0700

    corrected file naming conventions for plots

---
+++ anna_code/23andme/run_gwas/visualize.categorical.R
@@ -0,0 +1,29 @@
+rm(list=ls())
+library(qqman)
+args <- commandArgs(TRUE)
+fname=args[1]
+data=read.table(fname,header=TRUE,stringsAsFactors=FALSE)
+names(data)=c("CHR","SNP","BP","A1","TEST","NMISS","OR","STAT","P")
+data=data[data$TEST=="ADD",]
+data$P[data$P==0]=1e-100
+data$CHR[data$CHR=="X"]=23
+data$CHR[data$CHR=="Y"]=24
+data$CHR[data$CHR=="MT"]=25
+data$CHR=as.numeric(data$CHR)
+
+fieldname=strsplit(fname,'/')[[1]][2]
+data$FIELD=fieldname
+#remove all the NA's
+data=na.omit(data)
+
+manh_name=paste(fname,'.manh.png',sep='')
+qq_name=paste(fname,'.qq.png',sep='')
+png(manh_name)
+manhattan(data,main=fieldname)
+dev.off()
+png(qq_name)
+qq(data$P,main=fieldname)
+dev.off()
+#get the significant SNPs 
+sig_subset=data[data$P<5e-8,]
+write.table(sig_subset,file=paste(fieldname,"significant.csv",sep='.'),row.names = FALSE,col.names = TRUE,sep='\t',quote=FALSE)
