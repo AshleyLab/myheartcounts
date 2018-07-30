4209633d589b36fc61cee89075134e8d4804aa46
Anna Shcherbina <annashcherbina@gmail.com>
2018-07-30 13:13:10 -0700

    re-analyzing MHC RCT data per suggestions from Laura

--- anna_code/rct/time_effect.R
+++ anna_code/rct/time_effect.R
@@ -1,1 +1,9 @@
 #Try out suggestions from Laura, as defined in AshleyMHC.docx
+rm(list=ls())
+options(warn=-1)
+library(nlme)
+library(car)
+library(multcompView)
+library(lsmeans)
+library(rcompanion)
+
