112d30eb1eb16102c73d751a529605b1f2cb8e93
Anna Shcherbina <annashcherbina@gmail.com>
2018-12-11 15:01:31 -0800

    pandas merge of all survey data

---
+++ anna_code/summarize/merge_surveys_and_activity.py
@@ -0,0 +1,26 @@
+import pandas as pd 
+from functools import reduce 
+import argparse 
+
+def parse_args(): 
+    parser=argparse.ArgumentParser(description="Merge specified list of tables by specified ID; full join") 
+    parser.add_argument("--tables",nargs="+") 
+    parser.add_argument("--outf") 
+    parser.add_argument("--index",help="field to use for merging") 
+    return parser.parse_args() 
+
+def main(): 
+    args=parse_args() 
+    dfs=[] 
+    for f in args.tables: 
+        print(f) 
+        data=pd.read_table(f,header=0,sep='\t') 
+        dfs.append(data) 
+    print("loaded all data frames")     
+    print("starting join") 
+    df_final=reduce(lambda left,right: pd.merge(left,right,how='outer',on=args.index),dfs)
+    print("writing output file") 
+    df_final.to_csv(args.outf,sep='\t',header=True,index=False)
+
+if __name__=="__main__": 
+    main() 
