a0ff43442704df997e6da4261e26e38e10026be9
Anna Shcherbina <annashcherbina@gmail.com>
2018-12-28 12:17:04 -0800

    updated parsing code to support parsing of HealthKit Workout Tables

--- anna_code/motion_tracker_analysis/v2/get_activity_fraction_and_duration_appv2.healthkit.sh
+++ anna_code/motion_tracker_analysis/v2/get_activity_fraction_and_duration_appv2.healthkit.sh
@@ -1,34 +1,37 @@
 #!/bin/sh
 subject_set=$1
 #DataCollector 
-python get_activity_fraction_and_duration_appv2.py --tables /scratch/PI/euan/projects/mhc/data/tables/cardiovascular-HealthKitDataCollector-v1.tsv \
-    --synapseCacheDir /scratch/PI/euan/projects/mhc/data/synapseCache/ \
-    --out_prefixes parsed_HealthKitData.$subject_set \
-    --data_types health_kit_data_collector \
-    --subjects /scratch/PI/euan/projects/mhc/data/tables/subjects/hk/x$subject_set \
-    --aws_file_pickle /scratch/PI/euan/projects/mhc/data/aws_interventions.p \
-    --ab_test /scratch/PI/euan/projects/mhc/data/tables/cardiovascular-ABTestResults-v1.tsv \
-    --appVersion /scratch/PI/euan/projects/mhc/data/tables/appVersionWithDates.tsv
-
-
-#Workout Collector 
-#python get_activity_fraction_and_duration_appv2.py --tables /scratch/PI/euan/projects/mhc/data/tables/cardiovascular-HealthKitWorkoutCollector-v1.tsv \
+#python get_activity_fraction_and_duration_appv2.py --tables /scratch/PI/euan/projects/mhc/data/tables/cardiovascular-HealthKitDataCollector-v1.tsv \
 #    --synapseCacheDir /scratch/PI/euan/projects/mhc/data/synapseCache/ \
-#    --out_prefixes parsed_v2_HealthKitWorkout.$subject_set \
-#    --data_types health_kit_data_collector_aws \
-#    --subjects /scratch/PI/euan/projects/mhc/data/tables/v2_data_subset/subjects/healthkit_workout/x$subject_set \
-#    --map_aws_to_healthcodes /scratch/PI/euan/projects/mhc/data/tables/cardiovascular-AwsClientIdTask-v1.tsv \
-#    --aws_file_pickle /scratch/PI/euan/projects/mhc/data/aws_interventions.p
-    #--aws_files /scratch/PI/euan/projects/mhc/data/aws.all \
-    #----ab_test /scratch/PI/euan/projects/mhc/data/tables/cardiovascular-ABTestResults-v1.tsv
+#    --out_prefixes parsed_HealthKitData.$subject_set \
+#    --data_types health_kit_data_collector \
+#    --subjects /scratch/PI/euan/projects/mhc/data/tables/subjects/hk/x$subject_set \
+#    --aws_file_pickle /scratch/PI/euan/projects/mhc/data/aws_interventions.p \
+#    --ab_test /scratch/PI/euan/projects/mhc/data/tables/cardiovascular-ABTestResults-v1.tsv \
+#    --appVersion /scratch/PI/euan/projects/mhc/data/tables/appVersionWithDates.tsv
 
-
+#SleepCollector
 #python get_activity_fraction_and_duration_appv2.py --tables /scratch/PI/euan/projects/mhc/data/tables/cardiovascular-HealthKitSleepCollector-v1.tsv \
 #    --synapseCacheDir /scratch/PI/euan/projects/mhc/data/synapseCache/ \
 #    --out_prefixes parsed_v2_HealthKitSleep.$subject_set \
-#    --data_types health_kit_sleep_collector_aws \
+#    --data_types health_kit_sleep_collector \
 #    --subjects /scratch/PI/euan/projects/mhc/data/tables/v2_data_subset/subjects/healthkit_sleep/x$subject_set \
-#    --map_aws_to_healthcodes /scratch/PI/euan/projects/mhc/data/tables/cardiovascular-AwsClientIdTask-v1.tsv \
-#    --aws_file_pickle /scratch/PI/euan/projects/mhc/data/aws_interventions.p
-#    --aws_files /scratch/PI/euan/projects/mhc/data/aws.all \
-#    ----ab_test /scratch/PI/euan/projects/mhc/data/tables/cardiovascular-ABTestResults-v1.tsv
+#    --aws_file_pickle /scratch/PI/euan/projects/mhc/data/aws_interventions.p \
+#    --ab_test /scratch/PI/euan/projects/mhc/data/tables/cardiovascular-ABTestResults-v1.tsv \
+#    --appVersion /scratch/PI/euan/projects/mhc/data/tables/appVersionWithDates.tsv
+
+
+#Workout Collector 
+python get_activity_fraction_and_duration_appv2.py --tables /scratch/PI/euan/projects/mhc/data/tables/cardiovascular-HealthKitWorkoutCollector-v1.tsv \
+    --synapseCacheDir /scratch/PI/euan/projects/mhc/data/synapseCache/ \
+    --out_prefixes parsed_v2_HealthKitWorkout.$subject_set \
+    --data_types health_kit_workout_collector \
+    --subjects /scratch/PI/euan/projects/mhc/data/tables/v2_data_subset/subjects/healthkit_workout/x$subject_set \
+    --map_aws_to_healthcodes /scratch/PI/euan/projects/mhc/data/tables/cardiovascular-AwsClientIdTask-v1.tsv \
+    --aws_file_pickle /scratch/PI/euan/projects/mhc/data/aws_interventions.p \
+    --ab_test /scratch/PI/euan/projects/mhc/data/tables/cardiovascular-ABTestResults-v1.tsv \
+    --appVersion /scratch/PI/euan/projects/mhc/data/tables/appVersionWithDates.tsv
+    #--aws_files /scratch/PI/euan/projects/mhc/data/aws.all \
+    
+
+
