6136a0536623ded6e53701b230c75455c34c747d
Anna Shcherbina <annashcherbina@gmail.com>
2019-03-12 15:08:18 -0700

    23&Me imputation to UKBB around chest pain and joint problem loci

---
+++ anna_code/23andme/impute/extract_chrom2_ukbb.sh
@@ -0,0 +1,2 @@
+plink --bfile /oak/stanford/groups/euan/projects/ukbb/data/genetic_data/v2/plink_small/ukb_imp_chr2_v2 --extract range chr2range.txt --make-bed --out ukb_imp_chr2.subset
+
---
+++ anna_code/23andme/impute/extract_chrom3_ukbb.sh
@@ -0,0 +1,2 @@
+plink --bfile /oak/stanford/groups/euan/projects/ukbb/data/genetic_data/v2/plink_small/ukb_imp_chr3_v2 --extract range chr3range.txt --make-bed --out ukb_imp_chr3.subset
+
