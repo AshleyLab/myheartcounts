91fe0b772a14e6212963e913db779a2007312021
Anna Shcherbina <annashcherbina@gmail.com>
2019-04-17 02:04:28 -0700

    updated sleep parsing code to include healthKit Code in the annotation a and to extract summary features

--- anna_code/sleep_parser/parse_sleep_data.py
+++ anna_code/sleep_parser/parse_sleep_data.py
@@ -1,84 +1,66 @@
-import argparse 
+#parses healthkit sleep data to extract the following fields:
+
+#1) Mean time to bed
+#2) Mean time awake
+#3) Mean sleep duration
+#4) Mean sleep quality
+#5) N entries per subject
+#6) Std dev of #1,2,3,4
+#7) number sessions per person/day 
 import pandas as pd
-from os import listdir 
-from os.path import isfile,join 
-import numpy as np 
-from datetime import datetime,timedelta,date
 from dateutil.parser import parse
-import pdb 
 
-#do not record sleep entries < 1 hour in duration 
-min_time_interval=60*60
+#parse a single row in either the "sleep" or "bed" dataframe
+def parse_row(data_dict,row):
+    subject=row['healthCodeId']
+    #parse out the date component
+    startTime=parse(row['starttime'])
+    bedtime_hour=starTime.hour
+    waketime=startTime+timedelta(seconds=row['value'])
+    waketime_hour=waketime.hour
+    duration=row['value']
+    sourceId=row['sourceIdentifier']
+    session=(bedtime_hour,waketime_hour,duration,sourceId)
+    if subject not in data_dict:
+        data_dict[subject]=dict()
+    if day not in data_dict[subject]:
+        data_dict[subject][day]=[session]
+    else:
+        data_dict[subject][day].append(session)
+    return data_dict
 
-def parse_args(): 
-    parser=argparse.ArgumentParser(description="get time to bed, time out of bed, time to sleep, time awake") 
-    parser.add_argument("--healthKitSleepCollector",default="/scratch/PI/euan/projects/mhc/data/tables/cardiovascular-HealthKitSleepCollector-v1.tsv") 
-    parser.add_argument("--synapseCache",default="/scratch/PI/euan/projects/mhc/data/synapseCache") 
-    parser.add_argument("--subjects_file",default=None) 
-    parser.add_argument("--out_prefix",default="out") 
-    return parser.parse_args() 
-
-def main(): 
-    args=parse_args() 
-    data=pd.read_csv(args.healthKitSleepCollector,header=0,sep='\t',index_col=0)
-    print("loaded healthKitSleepCollector file") 
-
-    subjects=None 
-    if args.subjects_file is not None: 
-        subject_dict=dict() 
-        subjects=open(args.subject_file,'r').read().strip().split('\n') 
-        for line in subjects: 
-            subject_dict[subject]=1
-    print("got subject subset") 
+def parse_args():
+    parser=argparse.ArgumentParser(description="summarize HealthKit Sleep data")
+    parser.add_argument("--hk_sleep_df",default="")
+    parser.add_argument("--hk_inbed_df",default="")
+    parser.add_argument("--outf",default="sleep_data_summary.txt")
+    return parser.parse_args()
+def main():
+    #subject->date 
+    sleep_dict=dict()
+    inbed_dict=dict() 
+    args=parse_args()
+    outf=open(args.outf,'w')
+    #note: first column is healthCodeId
+    sleep_data=pd.read_table(args.hk_sleep_df,header=0,sep='\t',usecols=[0,1,2,3,4,5,6,7])
+    bed_data=pd.read_table(args.hk_inbed_df,header=0,sep='\t',usecols=[0,1,2,3,4,5,6,7])
+    print("read in datasets")
+    for index,row in sleep_data.iterrows():
+        parse_row(sleep_dict,row)
+    print("parsed the sleep dataframe")
+    for index,row in bed_data.iterrows():
+        parse_row(inbed_dict,row)
+    print("parsed in the inbed dataframe")
+    #aggregate per-subject data
+    subject_summary=dict()
+    device_summary=dict() 
+    for subject in sleep_dict.keys():
+        #get mean/stdev of values in a session:     session=(bedtime_hour,waketime_hour,duration,sourceId)
+        subject_days=[i for i in list(sleep_dict[subject].values())]
+        mean_bed_time=[s[0] for s in sleep_dict[subject]
     
-    #set up the metadata for parsing sleep 
-    print("set up blob datatype and field name dictionary") 
-    out_dict=dict() 
-    header=['startTime','type','categoryValue','value','unit','source','sourceIdentifier']
-    out_dict['HKCategoryValueSleepAnalysisInBed']=open(args.out_prefix+'.HKCategoryValueSleepAnalysisInBed','w')
-    out_dict['HKCategoryValueSleepAnalysisInBed'].write('\t'.join(header)+'\n')
-    out_dict['HKCategoryValueSleepAnalysisAsleep']=open(args.out_prefix+'.HKCategoryValueSleepAnalysisAsleep','w') 
-    out_dict['HKCategoryValueSleepAnalysisAsleep'].write('\t'.join(header)+'\n')
-    print("created dictionary of output files") 
-    num_parsed=0
-    nrows=str(data.shape[0]) 
-    for index,row in data.iterrows(): 
-        num_parsed+=1
-        if num_parsed%1000==0: 
-            print(str(num_parsed)+"/"+nrows)
-
-        cur_healthCode=row['healthCode'] 
-        if ((subjects!=None) and (cur_healthCode not in subject_dict)): 
-            continue 
-        try: 
-            blob=str(int(row['data.csv']))
-        except: 
-            continue 
-        blob_prefix=blob[-3::].lstrip('0')
-        if blob_prefix=="": 
-            blob_prefix="0"
-        #get all the blob files to load 
-        blob_prefix='/'.join([args.synapseCache,blob_prefix,blob])
-        blob_files=[f for f in listdir(blob_prefix) if isfile(join(blob_prefix,f))]
-        for blob_file in blob_files: 
-            file_path=join(blob_prefix,blob_file)
-            try:
-                data=pd.read_csv(file_path,sep=',',dtype='str',engine='c')
-                #print("loaded:"+str(file_path))
-            except:
-                print("There was a problem importing:"+str(file_path))
-                continue 
-            #parse the data frame for this blob 
-            try: 
-                for index,row in data.iterrows(): 
-                    datatype=row[2] 
-                    val=int(row[3]) 
-                    if val<min_time_interval: 
-                        continue 
-                    out_dict[datatype].write('\t'.join([str(i) for i in row])+'\n')
-            except:
-                print("There was a problem parsing:"+str(file_path))
-                
-if __name__=="__main__": 
-    main() 
-
+        
+    
+if __name__=="__main__":
+    main()
+    
