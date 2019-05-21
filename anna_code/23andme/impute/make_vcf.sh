08f53fe95b9ee3c4d80697158449fd2417edfea7
Anna Shcherbina <annashcherbina@gmail.com>
2019-05-21 09:42:50 -0700

    updated GWAS annotations

--- anna_code/23andme/impute/make_vcf.sh
+++ anna_code/23andme/impute/make_vcf.sh
@@ -1,13 +1,20 @@
-#plink --bfile ukb_imp_chr2.subset --recode vcf --out ukb_imp_chr2.subset.vcf
-#plink --bfile ukb_imp_chr3.subset --recode vcf --out ukb_imp_chr3.subset.vcf
-#plink --bfile 23andme.chr2.subset --recode vcf --out 23andme.chr2.subset.vcf
-#plink --bfile 23andme.chr3.subset --recode vcf --out 23andme.chr3.subset.vcf
+#!/bin/bash
 
 #full dataset
-# ignore positions with no Alt allele 
+# remove SNPs with maf < 0.01 in population 
+# remove SNPs not in Hardy Weinberg equilibrium 
 # change chromosome naming convention: 23 -> X, 24 -> Y, 25 -> XY, 26 -> MT 
 # generate vcf file without family id 
 
 #remove any duplicate SNPs
-plink --bfile 23andmeBinary --list-duplicate-vars suppress-first ids-only
-plink --bfile 23andmeBinary --mac 1 --output-chr M --exclude plink.dupvar --recode vcf-iid --out 23andmeBinary
+plink --bfile ../23andmeBinary --list-duplicate-vars suppress-first ids-only
+#change chrom names & filter to maf > 0.01 
+plink --bfile ../23andmeBinary --output-chr M  --maf 0.01 --exclude plink.dupvar --make-bed  --out 23andmeBinary.nodupvar.maf01.chromnames
+#exclude snps that have 0 genotyping rate 
+plink --bfile 23andmeBinary.nodupvar.maf01.chromnames --exclude exclude.txt --make-bed --out 23andmeBinary.nodupvar.maf01.chromnames.remove0genotypes
+#get the hardy weinberg annotation p-values 
+plink --bfile 23andmeBinary.nodupvar.maf01.chromnames.remove0genotypes --hardy 
+#remove SNPs out of hardy weinberg equilibrium 
+plink --bfile 23andmeBinary.nodupvar.maf01.chromnames.remove0genotypes --hwe midp 1e-7 --make-bed --out 23andmeBinary.nodupvar.maf01.chromnames.remove0genotypes.hwe1e-7
+#generate vcf file 
+plink --bfile  23andmeBinary.nodupvar.maf01.chromnames.remove0genotypes.hwe1e-7  --recode vcf-iid --out 23andmeBinary
