34dd299ff6e1414bb1c6374e12adaf9c8322c817
Anna Shcherbina <annashcherbina@gmail.com>
2020-01-17 18:11:26 -0800

    changed data prefix

--- anna_code/motion_tracker_analysis/v2/get_activity_fraction_and_duration_appv2.healthkit.sh
+++ anna_code/motion_tracker_analysis/v2/get_activity_fraction_and_duration_appv2.healthkit.sh
@@ -1,37 +1,38 @@
 #!/bin/sh
 subject_set=$1
+data_prefix=/oak/stanford/groups/euan/projects/mhc
 #DataCollector 
-#python get_activity_fraction_and_duration_appv2.py --tables /scratch/PI/euan/projects/mhc/data/tables/cardiovascular-HealthKitDataCollector-v1.tsv \
-#    --synapseCacheDir /scratch/PI/euan/projects/mhc/data/synapseCache/ \
+#python get_activity_fraction_and_duration_appv2.py --tables $data_prefix/mhc/data/tables/cardiovascular-HealthKitDataCollector-v1.tsv \
+#    --synapseCacheDir $data_prefix/mhc/data/synapseCache/ \
 #    --out_prefixes parsed_HealthKitData.$subject_set \
 #    --data_types health_kit_data_collector \
-#    --subjects /scratch/PI/euan/projects/mhc/data/tables/subjects/hk/x$subject_set \
-#    --aws_file_pickle /scratch/PI/euan/projects/mhc/data/aws_interventions.p \
-#    --ab_test /scratch/PI/euan/projects/mhc/data/tables/cardiovascular-ABTestResults-v1.tsv \
-#    --appVersion /scratch/PI/euan/projects/mhc/data/tables/appVersionWithDates.tsv
+#    --subjects $data_prefix/mhc/data/tables/subjects/hk/x$subject_set \
+#    --aws_file_pickle $data_prefix/mhc/data/aws_interventions.p \
+#    --ab_test $data_prefix/mhc/data/tables/cardiovascular-ABTestResults-v1.tsv \
+#    --appVersion $data_prefix/mhc/data/tables/appVersionWithDates.tsv
 
 #SleepCollector
-python get_activity_fraction_and_duration_appv2.py --tables /scratch/PI/euan/projects/mhc/data/tables/cardiovascular-HealthKitSleepCollector-v1.tsv \
-    --synapseCacheDir /scratch/PI/euan/projects/mhc/data/synapseCache/ \
+python get_activity_fraction_and_duration_appv2.py --tables $data_prefix/data/tables/cardiovascular-HealthKitSleepCollector-v1.tsv \
+    --synapseCacheDir $data_prefix/mhc/data/synapseCache/ \
     --out_prefixes HealthKitSleep \
     --data_types health_kit_sleep_collector \
-    --subjects /scratch/PI/euan/projects/mhc/data/tables/subjects/hk.sleep \
-    --aws_file_pickle /scratch/PI/euan/projects/mhc/data/aws_interventions.p \
-    --ab_test /scratch/PI/euan/projects/mhc/data/tables/cardiovascular-ABTestResults-v1.tsv \
-    --appVersion /scratch/PI/euan/projects/mhc/data/tables/appVersionWithDates.tsv
+    --subjects $data_prefix/mhc/data/tables/subjects/hk.sleep \
+    --aws_file_pickle $data_prefix/mhc/data/aws_interventions.p \
+    --ab_test $data_prefix/mhc/data/tables/cardiovascular-ABTestResults-v1.tsv \
+    --appVersion $data_prefix/mhc/data/tables/appVersionWithDates.tsv
 
 
 #Workout Collector 
-#python get_activity_fraction_and_duration_appv2.py --tables /scratch/PI/euan/projects/mhc/data/tables/cardiovascular-HealthKitWorkoutCollector-v1.tsv \
-#    --synapseCacheDir /scratch/PI/euan/projects/mhc/data/synapseCache/ \
+#python get_activity_fraction_and_duration_appv2.py --tables $data_prefix/mhc/data/tables/cardiovascular-HealthKitWorkoutCollector-v1.tsv \
+#    --synapseCacheDir $data_prefix/mhc/data/synapseCache/ \
 #    --out_prefixes HealthKitWorkout \
 #    --data_types health_kit_workout_collector \
-#    --subjects /scratch/PI/euan/projects/mhc/data/tables/subjects/hk.workout \
-#    --map_aws_to_healthcodes /scratch/PI/euan/projects/mhc/data/tables/cardiovascular-AwsClientIdTask-v1.tsv \
-#    --aws_file_pickle /scratch/PI/euan/projects/mhc/data/aws_interventions.p \
-#    --ab_test /scratch/PI/euan/projects/mhc/data/tables/cardiovascular-ABTestResults-v1.tsv \
-#    --appVersion /scratch/PI/euan/projects/mhc/data/tables/appVersionWithDates.tsv
-    #--aws_files /scratch/PI/euan/projects/mhc/data/aws.all \
+#    --subjects $data_prefix/mhc/data/tables/subjects/hk.workout \
+#    --map_aws_to_healthcodes $data_prefix/mhc/data/tables/cardiovascular-AwsClientIdTask-v1.tsv \
+#    --aws_file_pickle $data_prefix/mhc/data/aws_interventions.p \
+#    --ab_test $data_prefix/mhc/data/tables/cardiovascular-ABTestResults-v1.tsv \
+#    --appVersion $data_prefix/mhc/data/tables/appVersionWithDates.tsv
+    #--aws_files $data_prefix/mhc/data/aws.all \
     
 
 
