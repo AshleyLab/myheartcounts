a505bec6733e3327cc033ffe292e66263514b643
Anna Shcherbina <annashcherbina@gmail.com>
2020-04-25 16:59:11 -0700

    updated python parser

---
+++ downloads/python/download_blobs_syn3560085.sh
@@ -0,0 +1,4 @@
+python download.py --start_date 2019-07-26 \
+    --blobs_only \
+    --tables syn3560085
+
--- downloads/python/download_qc_healthcodes.sh
+++ downloads/python/download_qc_healthcodes.sh
@@ -1,3 +1,3 @@
-python download_python.py --healthCodes "WQnC6DNCKtUJesR3iQg7kX7" "nj0_2mSJGrRtLu7n7i8" "g3DPr9gGrzkxiZ1FzJqJCEvM" "\-7tzqFrFEohYxCtg9zEfVPk5" "eMUgFnVAjj1esrWybRqwsqLF"
+python download.py --healthCodes "WQnC6DNCKtUJesR3iQg7kX7" "nj0_2mSJGrRtLu7n7i8" "g3DPr9gGrzkxiZ1FzJqJCEvM" "\-7tzqFrFEohYxCtg9zEfVPk5" "eMUgFnVAjj1esrWybRqwsqLF"
 
 
