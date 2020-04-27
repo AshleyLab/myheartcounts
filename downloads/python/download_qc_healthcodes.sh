68920ca72edfb81a8647d6b7ca4e89a3aa2db91b
Anna Shcherbina <annashcherbina@gmail.com>
2020-04-27 14:34:12 -0700

    qc dataset download

--- downloads/python/download_qc_healthcodes.sh
+++ downloads/python/download_qc_healthcodes.sh
@@ -1,3 +1,4 @@
-python download.py --healthCodes "WQnC6DNCKtUJesR3iQg7kX7" "nj0_2mSJGrRtLu7n7i8" "g3DPr9gGrzkxiZ1FzJqJCEvM" "\-7tzqFrFEohYxCtg9zEfVPk5" "eMUgFnVAjj1esrWybRqwsqLF"
+python download.py --healthCodes "WQnC6DNCKtUJesR3iQg7kX7" "nj0_2mSJGrRtLu7n7i8\-WzkI" "g3DPr9gGrzkxiZ1FzJqJCEvM" "\-7tzqFrFEohYxCtg9zEfVPk5" "eMUgFnVAjj1esrWybRqwsqLF" --table_dir qc_healthcodes
+
 
 
